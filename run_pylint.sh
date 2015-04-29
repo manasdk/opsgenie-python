c44b740579b927ac0f3cf28278da214efa682467
Colton Leekley-Winslow <colton@hurricanelabs.com>
2015-04-29 17:08:45 -0400

    add test for list alerts

--- run_pylint.sh
+++ run_pylint.sh
@@ -1,1 +1,2 @@
+echo "Running pylint"
 find src/opsgenie -name "*.py" -exec pylint -E {} \+
--- tests/test_alert.py
+++ tests/test_alert.py
@@ -1,7 +1,9 @@
 import json
+import random
 import unittest
 import uuid
 
+import fauxfactory
 import responses
 
 from opsgenie.api import OpsGenieAPI
@@ -22,6 +24,7 @@ class TestAlertResource(unittest.TestCase):
             )
             test_message = str(uuid.uuid4())
             alert_response = self.api.get_resource("alert").create(test_message)
+            self.assertEqual(alert_response["status"], "successful")
             self.assertEqual(len(requests_mock.calls), 1)
             alert_request = json.loads(requests_mock.calls[0].request.body)
             check_request_vals = {
@@ -29,4 +32,45 @@ class TestAlertResource(unittest.TestCase):
             }
             for request_key, request_value in check_request_vals.items():
                 self.assertEqual(alert_request[request_key], request_value)
-            
+
+    def test_list(self):
+        fake_alerts_list = [self.generate_fake_alert() for x in range(0, random.randint(0,5))]
+        with responses.RequestsMock() as requests_mock:
+            requests_mock.add(
+                requests_mock.GET,
+                "https://api.opsgenie.com/v1/json/alert",
+                body=json.dumps({
+                    "alerts":fake_alerts_list
+                    })
+                status=200,
+                content_type="application/json"
+            )
+            alerts_response = self.api.get_resource("alert").list()
+            alerts_list = alerts_response["alerts"]
+            self.assertEqual(len(requests_mock.calls), 1)
+            self.assertEqual(alerts, fake_alerts_list)
+
+    def generate_fake_alert(self, **set_values):
+        alert = {
+            "id":str(uuid.uuid4()),
+            #No tinyId because you have no good reason to want that
+            "message":fauxfactory.gen_string("alphanumeric", random.randint(1,130)),
+            "alias":fauxfactory.gen_string("alphanumeric", random.randint(1,30)),
+            "description":fauxfactory.gen_string("alphanumeric", random.randint(1,15000)),
+            "recipients":[fauxfactory.gen_string("alphanumeric", random.randint(1,30)) 
+                for x in range(0, random.randint(0,5))],
+            #No actions at this time check back later
+            "source":fauxfactory.gen_string("alphanumeric", random.randint(1,30)),
+            "tags":",".join([fauxfactory.gen_string("alphanumeric", random.randint(1,30))
+                for x in range(0, random.randint(0,5))],
+            "details":{fauxfactory.gen_string("alphanumeric", random.randint(1,30), 
+                fauxfactory.gen_string("alphanumeric", random.randint(1,30) for x in range(
+                    0, random.randint(0,5))},
+            "entity":fauxfactory.gen_string("alphanumeric", random.randint(1,30)),
+            "user":fauxfactory.gen_string("alphanumeric", random.randint(1,30)),
+            "note":fauxfactory.gen_string("alphanumeric", random.randint(1,30))
+        }
+        for set_key, set_val in set_values.items():
+            alert[sey_key] = set_val
+        return alert
+        
